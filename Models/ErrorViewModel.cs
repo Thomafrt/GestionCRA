3778df7e2f582ef69210112f05e1c4e6d3e64984
Thomas <thom.foret@free.fr>
2023-12-07 23:26:34 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionCRA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
